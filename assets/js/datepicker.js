eed293354e2a92024726eb00951c97590f35ffd9
EDWIN ORBES <edwin20genius@gmail.com>
2016-04-07 10:38:53 -0500

    Mostrar la Semana del Año

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,18 +1,5 @@
 $(function () {
   $("#datepicker").datepicker({
+    showWeek: true
   });
 });
-
-// Funcion para Deshabilitar el Calendario
-$(function () {
-  $("#deshabilitar").click(function () {
-    $("#datepicker").datepicker('option','disabled',true);
-  })
-})
-
-// Función para Habilitar el Calendario
-$(function () {
-  $("#habilitar").click(function () {
-    $('#datepicker').datepicker('option','disabled',false);
-  })
-})
