1339d496e176d474f838a59e9a228907ef427ef7
EDWIN ORBES <edwin20genius@gmail.com>
2016-04-07 11:39:17 -0500

    Fijar una fecha de Visión

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,5 +1,12 @@
 $(function () {
   $("#datepicker").datepicker({
-    showButtonPanel: true
+    //showButtonPanel: true
+    defaultDate: "23/03/2000"
+  });
+});
+
+$(function () {
+  $("#enviar").click(function () {
+    $("#datepicker").datepicker("setDate","+5m");
   });
 });
