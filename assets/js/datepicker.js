cdff3bdfb7ab5e750f0c14703d03c8beb28edf3c
EDWIN ORBES <edwin20genius@gmail.com>
2016-04-08 00:08:25 -0500

    Final del Tutorial

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,17 +1,1 @@
-// Elige el Rango minimo para el otro Calendario
-$(function () {
-  $("#from").datepicker({
-    onClose: function (selectedDate) {
-      $("#to").datepicker("option","minDate",selectedDate);
-    }
-  });
-});
 
-// Elige el Rango maximo para el otro Calendario
-$(function () {
-  $("#to").datepicker({
-    onClose: function (selectedDate) {
-      $("#from").datepicker("option","maxDate",selectedDate);
-    }
-  });
-})
